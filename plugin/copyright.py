08fcdd88f7647cfa39fb1aefee31880591853bea
Francis Sun <francisjsun@outlook.com>
2020-01-14 21:51:52 +0800

    add submodule external/jupiter

--- plugin/copyright.py
+++ plugin/copyright.py
@@ -3,11 +3,11 @@ try:
 except ImportError:
     import subprocess
     import sys
-    subprocess.run([sys.executable, "-m", "pip", "install", \
-            "git+ssh://git@github.com/francisjsun/jupiter.git@master"])
+    subprocess.run([sys.executable, "-m", "pip", "install", "-e", \
+            "../external/jupiter"])
     from jupiter import copyright_utility
+
 import vim
-import argparse
 
 if __name__ == "__main__":
     cr = copyright_utility.Copyright(vim.eval("g:moon_plugin_copyright_file_path"), \
