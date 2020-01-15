086eabce43fab001fee6fdddf563678841764018
Francis Sun <francisjsun@outlook.com>
2020-01-15 18:16:27 +0800

    fix #issue pip install

--- plugin/copyright.py
+++ plugin/copyright.py
@@ -3,7 +3,7 @@ try:
 except ImportError:
     import subprocess
     import sys
-    subprocess.run([sys.executable, "-m", "pip", "install", "-e", \
+    subprocess.run([sys.executable, "-m", "pip", "install", "--user", "-e", \
             "../external/jupiter"])
     from jupiter import copyright_utility
 
@@ -12,4 +12,15 @@ import vim
 if __name__ == "__main__":
     cr = copyright_utility.Copyright(vim.eval("g:moon_plugin_copyright_file_path"), \
             vim.eval("g:moon_plugin_copyright_author"))
-    vim.command("let g:moon_plugin_copyright_doc = " + cr.Get())
+
+    # if doc contains any literal quotes, then thoes quotes should be escaped to the 
+    # final let command
+    doc = cr.Get()
+    raw_doc = ""
+    for c in doc:
+        if c == "\"":
+            raw_doc += "\\\""
+        else:
+            raw_doc += c
+
+    vim.command("let g:moon_plugin_copyright_doc = " + "\"" + raw_doc + "\"")
--- plugin/copyright.vim
+++ plugin/copyright.vim
@@ -4,20 +4,20 @@
 " g:moon_plugin_copyright_author, g:moon_plugin_copyright_file_path, 
 " g:moon_plugin_copyright_doc
 let g:moon_plugin_copyright_author = "unknown"
-
 if has("python3") != 1
   echoerr "python3 is not found."
   finish
 endif
 
+let s:here = expand('<sfile>:p:h')
+
 function! s:GetCopyrightDoc(file_path, author)
   let g:moon_plugin_copyright_file_path = a:file_path
   let g:moon_plugin_copyright_author = a:author
-  py3f copyright.py
+  execute 'py3f' s:here . '/copyright.py'
   return g:moon_plugin_copyright_doc
 endfunction
 
 function! moon#plugin#copyright#AddCopyrightToCurrentFile(author)
-  echom "add copyright to current file" . %
-  1insert Moon#plugin#copyright#get_copyright_doc(%, a:author)
+  call append(0, s:GetCopyrightDoc(expand('%:p'), a:author) . '\<CR>')
 endfunction
