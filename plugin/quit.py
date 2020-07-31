e5e81c0eed941038a9e63c6879b2718b33d1605b
Francis Sun <francisjsun@outlook.com>
2020-07-31 08:18:15 +0800

    Fix #issue error when can't write moon_cfg.json

--- plugin/quit.py
+++ plugin/quit.py
@@ -10,6 +10,10 @@ vim_moon_cfg = vim.eval("g:moon_cfg")
 moon_cfg_file_name = "moon_cfg.json"
 moon_cfg_file_path = os.path.join(vim.eval("g:vimrc_dir"), moon_cfg_file_name)
 if vim_moon_cfg['dirty']:
-    with open(moon_cfg_file_path, 'w') as moon_cfg_file:
-        vim_moon_cfg['dirty'] = 0
-        json.dump(vim_moon_cfg, moon_cfg_file)
+    try:
+        with open(moon_cfg_file_path, 'w') as moon_cfg_file:
+            vim_moon_cfg['dirty'] = 0
+            json.dump(vim_moon_cfg, moon_cfg_file)
+    except OSError as err:
+        print(err.strerror + "@" + err.filename)
+        vim.command("call input(\"Press Enter to quit.\")")
